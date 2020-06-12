dbe27340431f78d53b161a8f4de2382d167a8023
Jeffrey Horn <jeffreyh@zillowgroup.com>
2020-06-12 16:35:46 -0700

    show winner

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
-import React, { useRef, useState, useCallback } from "react";
+import React, { useRef, useState, useCallback, useEffect } from "react";
 import "./styles.css";
 
 import { Board } from "./Board";
 import Engine from "./Engine";
+import confetti from "./confetti";
+
+const UP = 38;
+const DOWN = 40;
+const LEFT = 37;
+const RIGHT = 39;
+const ESCAPE = 27;
 
 function useEngine() {
   const engine = useRef();
@@ -11,42 +18,78 @@ function useEngine() {
     engine.current = new Engine();
   }
 
-  const [layout, setLayout] = useState(engine.current.layout);
+  const [state, setState] = useState({
+    layout: engine.current.layout,
+    winner: {},
+  });
 
-  const move = useCallback((dir) => {
-    const {
-      antigonusMoved,
-      bearMoved,
-      winner,
-      layout: nextLayout,
-    } = engine.current.step(dir);
-
-    setLayout(nextLayout);
+  const reset = useCallback(() => {
+    engine.current = new Engine();
+    setState({ layout: engine.current.layout, winner: {} });
   }, []);
 
-  return [layout, move];
+  const move = useCallback((dir) => {
+    const { winner, layout: nextLayout } = engine.current.step(dir);
+
+    setState({ layout: nextLayout, winner });
+  }, []);
+
+  return [state, move, reset];
 }
 
 export default function App() {
+  const [winner, setWinner] = useState("");
   const [dir, setDir] = useState("");
-  const [layout, move] = useEngine();
+  const [state, move, reset] = useEngine();
+
+  const keyDown = useCallback((evt) => {
+    evt.preventDefault();
+    evt.stopPropagation();
+
+    switch (evt.keyCode) {
+      case UP:
+        move("N");
+        break;
+      case DOWN:
+        move("S");
+        break;
+      case LEFT:
+        move("W");
+        break;
+      case RIGHT:
+        move("E");
+        break;
+      case ESCAPE:
+        reset();
+        break;
+      default:
+        break;
+    }
+  }, []);
+
+  useEffect(() => {
+    if (state.winner.bear) {
+      setWinner("Bear");
+    } else if (state.winner.antigonus) {
+      setWinner("Antigonus");
+      confetti.start(1000);
+    } else {
+      setWinner("");
+    }
+  }, [state.winner]);
+
+  useEffect(() => {
+    document.addEventListener("keydown", keyDown);
+
+    return () => {
+      document.removeEventListener("keydown", keyDown);
+    };
+  }, [keyDown]);
 
   return (
     <div className="App">
-      <input
-        type="text"
-        value={dir}
-        onChange={(evt) => setDir(evt.target.value)}
-      />
-      <button
-        onClick={() => {
-          move(dir);
-          setDir("");
-        }}
-      >
-        Move
-      </button>
-      <Board layout={layout} move={move} />
+      <h1>{winner !== "" ? `The ${winner} wins!!` : "Who will win?"}</h1>
+      <Board layout={state.layout} />
     </div>
   );
 }
